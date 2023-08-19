ffd76950d770a37c88daa7cdefca25beb094878b
Raghav Aggarwal <aggarwalchirag2000@gmail.com>
2023-08-19 23:09:34 +0530

    .env changes

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use("/message", messageRoutes);
 
 const __dirname1 = path.resolve();
 
-if(process.env.NODE_ENV === "productions"){
+if(process.env.NODE_ENV === "production"){
 
     app.use(express.static(path.join(__dirname1, "/frontend/build")))
 
