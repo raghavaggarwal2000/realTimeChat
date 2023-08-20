3fc9e9b9a3f8b8bdc14b4d7d0f85b613ade1e989
Raghav Aggarwal <aggarwalchirag2000@gmail.com>
2023-08-20 10:40:11 +0530

    fix/ socket io endpoint in frontend

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -12,7 +12,8 @@ import io from "socket.io-client"
 import Lottie from "react-lottie";
 import animationData from "./animation/typing.json";
 
-const ENDPOINT = "http://localhost:5000";
+// const ENDPOINT = "http://localhost:5000"; // for local
+const ENDPOINT = "https://real-time-chat-xwu5.onrender.com"; // for deployed link
 let socket, selectedChatCompare;
 
 const SingleChat = ({fetchAgain, setFetchAgain}) => {
